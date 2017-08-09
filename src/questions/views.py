127032bfeebde94a36f6c2987875386d3a4f71a8
yemalin <gyemalin@gmail.com>
2017-08-08 23:32:21 -0500

    working on forms and questions

--- src/questions/views.py
+++ src/questions/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render, Http404
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import Http404
 from .models import Question, Answer 
 from .forms import UserResponseForm
 # Create your views here.
+
+
+def single(request, id):
+
+	if request.user.is_authenticated() :
+		form = UserResponseForm(request.POST or None)
+		if form.is_valid():
+			#print form.cleaned_data
+			print request.POST
+			question_id =form.cleaned_data.get('question_id')
+			answer_id =form.cleaned_data.get('answer_id')
+			question_instance = Question.objects.get(id = question_id)
+			answer_instance   = Answer.objects.get(id = answer_id)
+			print answer_instance.text, question_instance.text
+			next_q = Question.objects.order_by("?").first()
+			return redirect("question_single", id=next_q.id)
+
+		queryset = Question.objects.all().order_by('-timestamp') 
+		instance =get_object_or_404(Question, id=id)
+		context = {
+			"form": form,
+			"instance": instance,
+			#"queryset": queryset
+		}
+		return render(request, "questions/single.html", context)
+	else:
+		raise Http404
+
+
 def home(request):
 	if request.user.is_authenticated() :
 		form = UserResponseForm(request.POST or None)
