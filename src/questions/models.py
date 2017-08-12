c8ba42b910c9ef410abc36c1e3ea0e708fc28f24
yemalin <gyemalin@gmail.com>
2017-08-12 14:01:27 -0500

    match contiunes 0812

--- src/questions/models.py
+++ src/questions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Question(models.Model):
@@ -8,14 +9,35 @@ class Question(models.Model):
 	timestamp = models.DateTimeField(auto_now_add=True, auto_now = False)
 
 	def __unicode__(self):
-		return self.text[:10]
+		return self.text[:20]
 
 class Answer(models.Model):
-	question = models.ForeignKey(Question)
-	text   = models.CharField(max_length =120)
+	question  = models.ForeignKey(Question)
+	text      = models.CharField(max_length =120)
 	active    = models.BooleanField(default=True)
 	draft     = models.BooleanField(default=False)
 	timestamp = models.DateTimeField(auto_now_add=True, auto_now = False)
 
 	def __unicode__(self):
-		return self.text[:10]	
+		return self.text[:20]	
+
+
+
+LEVELS =(
+	('Mandatory' , 'Mandatory'),
+	('Very Important', 'Very Important'),
+	('Somewhat Important', 'Somewhat Important'),
+	('Not Important', 'Not Important'),
+	)
+
+class UserAnswer(models.Model):
+	user 						= models.ForeignKey(settings.AUTH_USER_MODEL)
+	question 					= models.ForeignKey(Question)
+	my_answer 					= models.ForeignKey(Answer, related_name='user_answer')
+	my_answer_importance		= models.CharField(max_length=50, choices =LEVELS)
+	their_answer 			    = models.ForeignKey(Answer, null=True, blank=True, related_name='match_answer')
+	their_answer_importance	    = models.CharField(max_length=50, choices =LEVELS)
+	timestamp 					= models.DateTimeField(auto_now_add=True, auto_now = False)
+
+	def __unicode__(self):
+		self.my_answer.text[:20]
